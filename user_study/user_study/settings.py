e46a0c9c12aa26f775811f2369eb15cecd10272e
Vittorio Pippi <219424@studenti.unimore.it>
2023-11-10 14:14:13 +0100

    Enable debug

--- user_study/user_study/settings.py
+++ user_study/user_study/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bx*vjw)i%a*ujll-1i@n%9)q*48@3ac@*o_7m%-p8uurw8md%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
